131c5a08c924fe26caa1644291d32ec5e199d20a
jsjhyun <gusrnl98@naver.com>
2024-02-03 17:03:18 +0900

    db 변경

---
+++ src/main/java/jpabook/jpashop/domain/CategoryItem.java
@@ -0,0 +1,31 @@
+package jpabook.jpashop.domain;
+
+
+import jakarta.persistence.*;
+import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "category_item")
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CategoryItem {
+
+    @Id @GeneratedValue
+    @Column(name = "category_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
+    private Item item;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+
+
+}
